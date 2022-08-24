8011c8fafeb6ac8c936612a6bf7d5f1217a97aed
sandhyasipan <sandhyaswain70@gmail.com>
2022-08-24 07:04:21 +0530

    added to editcontactUC3

--- AdresssBook.java
+++ AdresssBook.java
@@ -1,14 +1,19 @@
 package com.bridgelabs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class AdresssBook {
-    static ArrayList<Person> list = new ArrayList<>();
-    static Scanner scan = new Scanner(System.in);
+    static ArrayList<Person> list = new ArrayList();
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String args[]) {
         boolean isAdd = true;
         while (isAdd) {
             System.out.println("Press 1 for add contact :");
             System.out.println("Press 2 for Display contact :");
+            System.out.println("Press 3 for Edit contact : ");
             System.out.println("Press 0 for Exit contact : ");
             int choice = scanner.nextInt();
             switch (choice) {
@@ -17,6 +22,9 @@ public class AdresssBook {
                     break;
                 case 2:
                     displayPerson();
+                    break;
+                case 3:
+                    editPerson();
                     break;
                 default:
                     isAdd = false;
@@ -40,6 +48,24 @@ public class AdresssBook {
         System.out.println("___________________________________");
         System.out.println("List is :  " + list);
 
+    }
+    public static void editPerson() {
+        System.out.println("Please enter your first name");
+        String personName = scanner.next();
+        System.out.println("___________________________________");
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).getFirstname().equals(personName)){
+                System.out.println("Please Enter first Name");
+                String firstname = scanner.next();
+                System.out.println("Enter last Name");
+                String lastname = scanner.next();
+                System.out.println("Enter address");
+                String address = scanner.next();
+                list.get(i).getFirstname();
+                list.get(i).getLastname();
+                list.get(i).setAddress(address);
+            }
+        }
     }
 }
 
